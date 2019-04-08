25ef75090bbfa38c790057c7b97f2f0eb3449ade
RosMEW <rosalie.yue.liu@gmail.com>
2019-04-07 20:02:49 -0400

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
+const App = () => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className='Container'>
+            <h1>Section 1</h1>
+        </div>
     );
-  }
-}
+};
 
 export default App;
