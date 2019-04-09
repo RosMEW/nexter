b8cb4255d2f9e936645da2da4b83a0956a81a946
RosMEW <rosalie.yue.liu@gmail.com>
2019-04-09 16:09:54 -0400

    Add Description Section

--- src/DescriptionSection/DescriptionSection.tsx
+++ src/DescriptionSection/DescriptionSection.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './DescriptionSection.scss';
+import '../style/typography.scss';
+import { qualitiesData } from '../shared/helpers';
+import { IQuality } from './Quality.model';
 
 const DescriptionSection = () => {
-    return <div className='Description'>Description</div>;
+    const [qualities, setqualities] = useState([] as IQuality[]);
+    useEffect(() => setqualities(qualitiesData()), []);
+
+    const qualitiesList = qualities.map(quality => (
+        <div className='Quality' key={quality.id}>
+            <svg className='Quality__icon'>
+                <use xlinkHref={`/images/sprite.svg#icon-${quality.icon}`} />
+            </svg>
+            <h4 className='heading-4 heading-4--dark'>{quality.heading}</h4>
+            <p className='Quality__text'>{quality.text}</p>
+        </div>
+    ));
+
+    return <section className='Description'>{qualitiesList}</section>;
 };
 
 export default DescriptionSection;
---
+++ src/DescriptionSection/Quality.model.tsx
@@ -0,0 +1,6 @@
+export type IQuality = {
+    id: string;
+    icon: string;
+    heading: string;
+    text: string;
+};
